04a232f73a8bb6df36b819dcaabef59da0e37d0a
ValerioViteritti <vviteritti@gmail.com>
2024-09-10 16:26:28 +0200

    feat: header

---
+++ resources/views/partials/header.blade.php
@@ -0,0 +1,37 @@
+<header>
+    <nav>
+        <div class="container">
+            <div class="row">
+                <img src="img/dc-logo.png" alt="Logo DC Comics">
+                <div id="linkNav">
+                    <ul>
+                        <li>
+                            <a href="#">Character</a>
+                            <a href="#">Comics</a>
+                            <a href="#">Movies</a>
+                            <a href="#">Tv</a>
+                            <a href="#">Games</a>
+                            <a href="#">Coleectibles</a>
+                            <a href="#">Videos</a>
+                            <a href="#">Fans</a>
+                            <a href="#">News</a>
+                            <a href="#">Shop</a>
+                        </li>
+                    </ul>
+                    {{-- <div v-for="(links, index) in links" :key="index">
+                        <a href="#">
+                            {{ links . text }}
+                        </a>
+                    </div> --}}
+
+
+                </div>
+
+            </div>
+        </div>
+    </nav>
+    <div class="jumbotron">
+        <div class="current">CURRENT SERIES</div>
+    </div>
+
+</header>
